12f5809c0048dfb43f01b562aad5833fb19d42a5
Shuvo <mh.shuvo.1167061@gmail.com>
2021-05-23 02:20:11 +0600

    sea ns sky texture added

--- Island_home/Island.cpp
+++ Island_home/Island.cpp
@@ -9,29 +9,172 @@
 #define PI 3.1415927
 
 #include "func.h"
+#include "BmpLoader.h"
 
 void sea()
 {
     glPushMatrix();
+    LoadTexture("C:\\Users\\HP\\Desktop\\GLUT_project\\Island_home\\sea.bmp", ID1);
+    glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
     glTranslatef(0,0,0);
     glScalef(50,2,50);
-    drawCube(0,0,0.80);
+    glClearColor(0,0,0,1);
+    drawCube(1,1,1);
     glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+    glPopMatrix();
+
 
 }
 
 void sky()
 {
     glPushMatrix();
+    LoadTexture("C:\\Users\\HP\\Desktop\\GLUT_project\\Island_home\\sky.bmp", ID2);
+     glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
     glTranslatef(0,0,0);
     glScalef(1,100,50);
-    drawCube(0.1,0.1,0.70);
+    glClearColor(0,0,0,1);
+    drawCube(1,1,1);
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
     glPopMatrix();
 
     glPushMatrix();
+    LoadTexture("C:\\Users\\HP\\Desktop\\GLUT_project\\Island_home\\sky.bmp", ID2);
+     glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
     glTranslatef(0,0,0);
     glScalef(50,100,1);
-    drawCube(0.1,0.1,0.70);
+    glClearColor(0,0,0,1);
+    drawCube(1,1,1);
     glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+     glPopMatrix();
+
+
+
+}
+
+void island_shape()
+{
+
+    GLfloat no_mat[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat mat_ambient[] = { 0, 1, 0, 1.0 };
+    GLfloat mat_diffuse[] = { 0, 1, 0, 1.0 };
+    GLfloat mat_specular[] = {1,1,1, 1.0 };
+    GLfloat mat_shininess[] = {60};
+     if(amb)
+    {
+    glMaterialfv( GL_FRONT, GL_AMBIENT, mat_ambient);
+
+    }
+    else
+    {
+        glMaterialfv( GL_FRONT, GL_AMBIENT, no_mat);
+    }
+
+    if(dif)
+    {
+    glMaterialfv( GL_FRONT, GL_DIFFUSE, mat_diffuse);
+
+    }
+    else
+    {
+        glMaterialfv( GL_FRONT, GL_DIFFUSE, no_mat);
+    }
+
+    if(spec)
+    {
+        glMaterialfv( GL_FRONT, GL_SPECULAR, mat_specular);
+    }
+    else
+    {
+        glMaterialfv( GL_FRONT, GL_SPECULAR, no_mat);
+    }
+
+
+
+    glMaterialfv( GL_FRONT, GL_SHININESS, mat_shininess);
+
+
+    glPushMatrix();
+    glRotatef(10,0,1,0);
+
+    glBegin(GL_TRIANGLES);
+        glVertex3f(20,0,20);
+        glVertex3f(20,0,30);
+        glVertex3f(30,0,30);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+        glVertex3f(20,5,20);
+        glVertex3f(20,5,30);
+        glVertex3f(30,5,30);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glVertex3f(20,0,20);
+        glVertex3f(20,0,30);
+         glVertex3f(20,5,30);
+        glVertex3f(20,5,20);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glVertex3f(20,0,20);
+        glVertex3f(30,0,30);
+         glVertex3f(30,5,30);
+        glVertex3f(20,5,20);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glVertex3f(20,0,30);
+        glVertex3f(30,0,30);
+         glVertex3f(30,5,30);
+        glVertex3f(20,5,30);
+    glEnd();
+
+    glPushMatrix();
+    ///glTranslatef(15,0,8);
+    ///glScalef(0.5,1,0.5);
+    ///glRotatef(20,0,1,0);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(24,0,30);
+        glVertex3f(24,0,37);
+        glVertex3f(30,0,37);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+        glVertex3f(24,5,30);
+        glVertex3f(24,5,37);
+        glVertex3f(30,5,37);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glVertex3f(20,0,20);
+        glVertex3f(20,0,30);
+         glVertex3f(20,5,30);
+        glVertex3f(20,5,20);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glVertex3f(20,0,20);
+        glVertex3f(30,0,30);
+         glVertex3f(30,5,30);
+        glVertex3f(20,5,20);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glVertex3f(20,0,30);
+        glVertex3f(30,0,30);
+         glVertex3f(30,5,30);
+        glVertex3f(20,5,30);
+    glEnd();
+
+    glPopMatrix();
+    glPopMatrix();
+
 
 }
